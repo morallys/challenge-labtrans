e5831faa3f93af38edf12198489212e0cdb5ed36
morallys <morallys.juan@gmail.com>
2022-12-11 13:46:17 -0300

    fix(src/index.ts): Insere um das rotas no index.ts

--- petshop/src/index.ts
+++ petshop/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import useRoutes from './routes';
+
 const PORT = 3535;
 const app = express();
 app.use(express.json());
 
+useRoutes(app);
+
 app.listen(PORT, () =>
-  console.log(`Server rodando com sucesso na porta ${PORT}`)
+  console.log(`Servidor rodando com sucesso na porta ${PORT}`)
 );
