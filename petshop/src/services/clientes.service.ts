1082d153dae6f0ff476ebf8263b4b5fd74e5701d
morallys <morallys.juan@gmail.com>
2022-12-11 14:38:02 -0300

    feat(services/clientes): Cria busca de usuário por ID

--- petshop/src/services/clientes.service.ts
+++ petshop/src/services/clientes.service.ts
@@ -1,6 +1,8 @@
 import { Prisma, PrismaClient } from '@prisma/client';
+import { StatusCodes } from 'http-status-codes';
 
-import Err from '../middlewares/errorMiddlewares';
+import Err from '../utils/Err';
+import errorMessage from '../utils/errorMessages';
 
 const prisma = new PrismaClient();
 
@@ -8,6 +10,22 @@ const getAll = async () => {
   return prisma.cliente.findMany();
 };
 
+const getById = async (id: String) => {
+  //
+  const convertId = Number(id);
+
+  if (Number.isNaN(convertId)) {
+    throw new Err(StatusCodes.BAD_REQUEST, errorMessage.number);
+  }
+
+  return prisma.cliente.findUnique({
+    where: {
+      id: convertId,
+    },
+  });
+};
+
 export default {
   getAll,
+  getById,
 };
