48c82a76331d5b9487602320ce4c7c98e4978bbe
morallys <morallys.juan@gmail.com>
2022-12-11 18:35:05 -0300

    feat(utils/errorMessages): Adiciona mensagens de erro personalizada

--- petshop/src/utils/errorMessages.ts
+++ petshop/src/utils/errorMessages.ts
@@ -1,5 +1,14 @@
 const errorMessage = {
-  number: 'Id must be a number',
+  allFields: 'Todos os campos devem ser preenchidos',
+  invalid: 'Deve ser um e-mail válido',
+  number: 'Deve ser um número',
+  string: 'Deve ser uma string',
+  invalidCpf: 'Deve ser um CPF válido',
+  nameLenght: 'O nome deve ter 5 caracteres no mínimo',
+  streetLenght: 'A rua deve ter 5 caracteres no mínimo',
+  clientNotFound: 'Nenhum cliente encontrado!!',
+  invalidPhone: 'Deve ser um telefone válido com 11 caracteres',
+  conflict: 'Os dados já existem no banco de dados',
 };
 
 export default errorMessage;
