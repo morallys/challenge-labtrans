4e40bfcdfd503dd97cf4732495cd508d5795df7c
morallys <morallys.juan@gmail.com>
2022-12-11 18:38:54 -0300

    feat(routes/clientes): Adiciona rota POST para add clientes
    - Adiciona middlewares de validação dos dados inseridos no body da requisição

--- petshop/src/routes/clientes.routes.ts
+++ petshop/src/routes/clientes.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import clientesController from '../controllers/clientes.controller';
+import clientesMiddleware from '../middlewares/clientes.middleware';
 
 const router = Router();
 
 router
   .get('/', clientesController.getAll)
-  .get('/:id', clientesController.getById);
+  .get('/:id', clientesController.getById)
+  .post('/', clientesMiddleware.checkBody, clientesController.addClient);
 
 export default router;
