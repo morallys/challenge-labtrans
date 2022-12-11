2c90db4689f2ea40214b894870025c4f65a4cd26
morallys <morallys.juan@gmail.com>
2022-12-11 13:46:51 -0300

    feat(routes/index): Adiciona rota de clientes ao index

--- petshop/src/routes/index.ts
+++ petshop/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express';
 
+import ClientesRoutes from './clientes.routes';
 import errorMiddleware from '../middlewares/errorMiddlewares';
 
 const routes = (app: Express) => {
+  app.use('/clientes', ClientesRoutes);
+
   app.use(errorMiddleware);
 };
 
