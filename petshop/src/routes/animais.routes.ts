0283f33a719ead3b1891df5a6a3e0954febcd3ff
morallys <morallys.juan@gmail.com>
2022-12-11 23:05:41 -0300

    feat(routes/animais): Cria rotas GET e POST para buscar por id e add

--- petshop/src/routes/animais.routes.ts
+++ petshop/src/routes/animais.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import routes from '.';
 
 import animaisController from '../controllers/animais.controller';
 
 const router = Router();
 
-router.get('/', animaisController.getAll);
+router
+  .get('/', animaisController.getAll)
+  .get('/:id', animaisController.getById)
+  .post('/', animaisController.addAnimal);
 
 export default router;
