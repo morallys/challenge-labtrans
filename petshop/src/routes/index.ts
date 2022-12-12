9a80eb7b7ded875de5840f9b7f644638b796c594
morallys <morallys.juan@gmail.com>
2022-12-11 21:16:00 -0300

    feat(routes/index): Adiciona rota para /animais

--- petshop/src/routes/index.ts
+++ petshop/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
 
 import ClientesRoutes from './clientes.routes';
+import animaisRoutes from './animais.routes';
 import errorMiddleware from '../middlewares/errorMiddlewares';
 
 const routes = (app: Express) => {
   app.use('/clientes', ClientesRoutes);
+  app.use('/animais', animaisRoutes);
 
   app.use(errorMiddleware);
 };
