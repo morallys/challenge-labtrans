83008879a3301faf3ae6ad491558015ad55a2bbf
morallys <morallys.juan@gmail.com>
2022-12-11 23:03:48 -0300

    feat(services/animais): Cria funções para adicionar e buscar por id

--- petshop/src/services/animais.service.ts
+++ petshop/src/services/animais.service.ts
@@ -12,6 +12,44 @@ const getAll = async () => {
   return prisma.animal.findMany();
 };
 
+const getById = async (id: string) => {
+  //
+  const convertId = Number(id);
+
+  if (Number.isNaN(convertId)) {
+    throw new Err(StatusCodes.BAD_REQUEST, `Id ${errorMessage.number}`);
+  }
+
+  return prisma.animal.findUnique({
+    where: {
+      id: convertId,
+    },
+  });
+};
+
+const addAnimal = async (animalData: Animal) => {
+  //
+  const { cliente_id } = animalData;
+
+  const cliente = await prisma.cliente.findUnique({
+    where: { id: cliente_id },
+  });
+
+  if (!cliente)
+    throw new Err(
+      StatusCodes.NOT_FOUND,
+      `${errorMessage.clientNotFound} com esse id.`
+    );
+
+  const animal = await prisma.animal.create({
+    data: animalData,
+  });
+
+  return animal;
+};
+
 export default {
   getAll,
+  getById,
+  addAnimal,
 };
